533bcba7609237d78549ba7e8a511daa3507557e
OpmTwo <OnePunchManTwo@outlook.com>
2025-11-07 20:49:43 +0530

    added base home sectio layout with the letter o

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,71 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<section id="section-intro" className="onscroll fitscreen seen loaded">
+			<div className="layer back">
+				<div className="rope sketch" path-speed="4" path-offset="0">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						id="svg-line1"
+						viewBox="0 0 2.663384 1080"
+						preserveAspectRatio="xMidYMax meet"
+					>
+						<path id="path-line1" d="M 1.331692,540 V 1080"></path>
+					</svg>
+				</div>
+			</div>
+
+			<div className="letter">
+				{/* BACK O */}
+				<div className="letter-back sketch">
+					<svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+						<path
+							path-onseen="1"
+							stroke="white"
+							strokeWidth="50"
+							fill="none"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							// Full O, centered in the viewBox
+							d="M 150 50 A 100 100 0 1 1 149.999 50 Z"
+							className="triggered"
+						></path>
+					</svg>
+				</div>
+
+				{/* MID O + VIDEO */}
+				<div className="letter-mid sketch">
+					<svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+						<path
+							path-onseen="1"
+							stroke="black"
+							strokeWidth="50"
+							fill="none"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M 150 50 A 100 100 0 1 1 149.999 50 Z"
+							className="triggered"
+						></path>
+					</svg>
+					<div className="video">
+						<video disablePictureInPicture muted loop autoPlay>
+							<source src="https://crustac.fr/wp-content/themes/crustac/img/video_waves3.mp4" type="video/mp4" />
+						</video>
+					</div>
+				</div>
+			</div>
+
+			<div className="layer loading"></div>
+
+			<a href="#" className="next-screen a-input scrollto" data-target="section-about">
+				<span className="in">Scroll</span>
+			</a>
+
+			<div className="pagination">
+				<div className="current">01</div>
+				<div className="total">07</div>
+			</div>
+
+			<div></div>
+		</section>
+	);
 }
