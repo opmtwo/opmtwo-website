c0f1c40a0cf57332300ecc7f9b347540aec9debb
OpmTwo <OnePunchManTwo@outlook.com>
2025-11-26 16:51:57 +0530

    added icons

---
+++ icons/EventBridgeIcon.tsx
@@ -0,0 +1,59 @@
+export function EventBridgeIcon(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" {...props}>
+			<g>
+				<path
+					strokeLinecap="round"
+					d="M11.5 2.05h7l1.426 3.157M2.422 15.207 1 12.05l2.3-5.2"
+					style={{
+						stroke: '#000',
+						strokeWidth: 1,
+						strokeDasharray: 'none',
+						strokeLinecap: 'round',
+						strokeDashoffset: 0,
+						strokeLinejoin: 'round',
+						strokeMiterlimit: 10,
+						fill: 'none',
+						fillRule: 'nonzero',
+						opacity: 1,
+					}}
+					transform="matrix(.83 0 0 .83 2.04 2.04)"
+				/>
+				<path
+					strokeLinecap="round"
+					d="M20.7 9.05a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM17.6 23.25a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM6.4 4.75a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM3.2 19.05a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM13.9 15.05H10l-1.3-3 1.3-3h3.9l1.4 3-1.4 3ZM13.9 15.05l2.674 4.502M10 9.05 7.403 4.475"
+					style={{
+						stroke: '#000',
+						strokeWidth: 1,
+						strokeDasharray: 'none',
+						strokeLinecap: 'round',
+						strokeDashoffset: 0,
+						strokeLinejoin: 'round',
+						strokeMiterlimit: 10,
+						fill: 'none',
+						fillRule: 'nonzero',
+						opacity: 1,
+					}}
+					transform="matrix(.83 0 0 .83 2.04 2.04)"
+				/>
+				<path
+					strokeLinecap="round"
+					d="M12.4 22.05H5.5l-1.406-3.21M21.598 8.84 23 12.05l-2.2 4.9"
+					style={{
+						stroke: '#000',
+						strokeWidth: 1,
+						strokeDasharray: 'none',
+						strokeLinecap: 'round',
+						strokeDashoffset: 0,
+						strokeLinejoin: 'round',
+						strokeMiterlimit: 10,
+						fill: 'none',
+						fillRule: 'nonzero',
+						opacity: 1,
+					}}
+					transform="matrix(.83 0 0 .83 2.04 2.04)"
+				/>
+			</g>
+		</svg>
+	);
+}
