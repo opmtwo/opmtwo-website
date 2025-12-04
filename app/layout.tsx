27505f8a7aafd16bf788759d6a958d8bdd101fe1
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-04 13:50:13 +0530

    Update font stylesheet links in RootLayout - added font tuffy

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Elms+Sans:ital,wght@0,100..900;1,100..900&family=Stack+Sans+Headline:wght@200..700&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Stack+Sans+Headline:wght@200..700&family=Tuffy:ital,wght@0,400;0,700;1,400;1,700&display=swap"
 					rel="stylesheet"
 				/>
 			</head>
