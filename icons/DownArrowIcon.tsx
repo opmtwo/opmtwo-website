9e83361a7ec0d300217ecde80f73c358cb10ebbc
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-06 16:27:51 +0530

    Add DownArrowIcon component as an SVG icon

---
+++ icons/DownArrowIcon.tsx
@@ -0,0 +1,26 @@
+const DownArrowIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg xmlns="http://www.w3.org/2000/svg" width={800} height={800} className="icon flat-line" data-name="Flat Line" viewBox="0 0 24 24" {...props}>
+		<path
+			d="M12 3v18"
+			style={{
+				fill: 'none',
+				stroke: '#000',
+				strokeLinecap: 'round',
+				strokeLinejoin: 'round',
+				strokeWidth: 2,
+			}}
+		/>
+		<path
+			d="m9 18 3 3 3-3"
+			data-name="primary"
+			style={{
+				fill: 'none',
+				stroke: '#000',
+				strokeLinecap: 'round',
+				strokeLinejoin: 'round',
+				strokeWidth: 2,
+			}}
+		/>
+	</svg>
+);
+export default DownArrowIcon;
