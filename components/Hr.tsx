6aa8c4173e97c4ce61847f374dda583d5c460cf1
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-06 16:06:14 +0530

    Add Hr component for horizontal rule with responsive styling

---
+++ components/Hr.tsx
@@ -0,0 +1,7 @@
+const Hr = () => {
+	return (
+		<div className="h-px w-full my-16 bg-linear-to-r from-transparent via-sky-500/40 to-transparent opacity-70 transition-all group-hover:via-emerald-400/70 group-hover:opacity-100"></div>
+	);
+};
+
+export default Hr;
