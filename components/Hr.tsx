291d9a62d2e7d348ba192921bfd4c73a531c3d41
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-08 19:55:09 +0530

    Updated HR - ruler blends to right

--- components/Hr.tsx
+++ components/Hr.tsx
@@ -1,6 +1,8 @@
 const Hr = () => {
 	return (
-		<div className="h-px w-full my-16 bg-linear-to-r from-transparent via-sky-500/40 to-transparent opacity-70 transition-all group-hover:via-emerald-400/70 group-hover:opacity-100"></div>
+		<>
+			<div className="h-px w-full my-16 bg-linear-to-r from-sky-500/80 via-sky-500/50 to-transparent opacity-90"></div>
+		</>
 	);
 };
 
