98f1fdacd886ad6e86d9ef98126fc29672deb041
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-09 14:58:41 +0530

    added meta exports in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,6 +13,15 @@ import ServicesShowcase from '@/components/ServicesShowcase';
 import { TechStack } from '@/components/TechStack';
 import TestimonialsSection from '@/components/TestimonialsSection';
 
+export const metadata = {
+	title: 'Elite AWS & AI Consultant | Serverless, Automation, Scale, Cost Savings',
+	description: `
+		Modernize legacy systems, eliminate bottlenecks, automate workflows, reduce costs and accelerate delivery.
+		From architecture to automation — launch world-class cloud solutions effortlessly with a seasoned AWS architect.
+		Trusted by global teams.
+	`,
+};
+
 export default function Home() {
 	return (
 		<>
