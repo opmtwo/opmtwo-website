b3e22d87d2ce220cad12f78c7a1fd2f95382df32
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-09 15:09:41 +0530

    Extracted home page to it's component

---
+++ app/pages/Home.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import CaseStudiesSection from '@/components/CaseStudiesSection';
+import FaqSection from '@/components/FaqSection';
+import ReadyToTrySection from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Hr from '@/components/Hr';
+import Nav from '@/components/Nav';
+import ProblemSolutionSection from '@/components/ProblemSolutionSection';
+import ScrollToNextSection from '@/components/ScrollToNextSection';
+import ServicesShowcase from '@/components/ServicesShowcase';
+import { TechStack } from '@/components/TechStack';
+import TestimonialsSection from '@/components/TestimonialsSection';
+
+export default function Home() {
+	return (
+		<>
+			<main className="min-h-screen">
+				<Nav />
+
+				<ScrollToNextSection />
+
+				<Hero />
+				<Hr />
+
+				<ProblemSolutionSection />
+				<Hr />
+
+				<CaseStudiesSection />
+				<Hr />
+
+				<ServicesShowcase />
+				<Hr />
+
+				<TestimonialsSection />
+				<Hr />
+
+				<TechStack />
+				<Hr />
+
+				<FaqSection />
+
+				<ReadyToTrySection />
+			</main>
+		</>
+	);
+}
