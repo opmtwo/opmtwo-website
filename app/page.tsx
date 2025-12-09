41ca0b94f078470b761fb2b1da20f655cea87476
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-09 15:16:04 +0530

    Updated home page - updated title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import HomeClient from '@/app/pages/Home';
 
 export const metadata: Metadata = {
-	title: 'Elite AWS & AI Consultant | Serverless, Automation, Scale, Cost Savings',
+	title: 'Fiverrs Pick AWS & AI Consultant | Cloud Modernization, AI Integrations & Serverless Engineering',
 	description: `
 		Modernize legacy systems, eliminate bottlenecks, automate workflows, reduce costs and accelerate delivery.
 		From architecture to automation — launch world-class cloud solutions effortlessly with a seasoned AWS architect.
