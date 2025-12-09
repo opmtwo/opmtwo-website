de2c8c7433b36a1ae14b42040fc576cee31046cd
OpmTwo <OnePunchManTwo@outlook.com>
2025-12-09 15:10:07 +0530

    Updated home page - imported HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import { redirect } from 'next/navigation';
+// app/page.tsx  (SERVER component)
+import type { Metadata } from 'next';
+import HomeClient from '@/app/pages/Home';
 
-import Home from '@/app/home/page';
+export const metadata: Metadata = {
+	title: 'Elite AWS & AI Consultant | Serverless, Automation, Scale, Cost Savings',
+	description: `
+		Modernize legacy systems, eliminate bottlenecks, automate workflows, reduce costs and accelerate delivery.
+		From architecture to automation — launch world-class cloud solutions effortlessly with a seasoned AWS architect.
+		Trusted by global teams.
+	`,
+};
 
-export default function Page() {
-	// redirect('/home');
-    return (
-        <>
-            <Home />
-        </>
-    )
+export default function HomePage() {
+	return <HomeClient />;
 }
